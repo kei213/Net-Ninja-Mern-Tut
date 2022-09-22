b3af2a38eddae2bd30523cead2d8b3cdb3ef3980
kei213 <kzimaman@gmail.com>
2022-09-22 14:43:50 +0200

    controllers

--- backend/routes/workoutRoutes.js
+++ backend/routes/workoutRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const Workout = require('../models/workoutModel')
+const { createWorkout } = require('../controllers/workoutController')
 
 const router = express.Router()
 
@@ -14,17 +14,7 @@ router.get('/:id', (req, res) => {
 })
 
 //Post a new workout
-router.post('/', async (req, res) => {
-	const {title, load, reps} = req.body
-
-	try {
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-	} catch (error) {
-        res.status(400).json({error: error.message})
-	}
-
-})
+router.post('/', createWorkout)
 
 //Delete a workout
 router.delete('/:id', (req, res) => {
