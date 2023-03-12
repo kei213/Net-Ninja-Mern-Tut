43a5bb4179e349d8e4d4ede10f84bc1b455bec42
unknown <kzimaman@gmail.com>
2023-03-12 12:10:29 +0200

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="fb-root">
+          <div class="fb-comments" data-href="https://developers.facebook.com/docs/plugins/comments#configurator" data-width="" data-numposts="5"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
